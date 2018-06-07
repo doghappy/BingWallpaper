1087c4203357071e388e466d9e888cdfc0311031
HeroWong <hero_wong@outlook.com>
2018-06-07 10:14:48 +0800

    add loading state

--- HappyDog.BingWallpaper/Views/ImageListPage.xaml.cs
+++ HappyDog.BingWallpaper/Views/ImageListPage.xaml.cs
@@ -2,23 +2,38 @@
 using HappyDog.BingWallpaper.Models;
 using HappyDog.BingWallpaper.Services;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 
 namespace HappyDog.BingWallpaper.Views
 {
-    public sealed partial class ImageListPage : Page
+    public sealed partial class ImageListPage : Page, INotifyPropertyChanged
     {
         public ImageListPage()
         {
             InitializeComponent();
+            IsLoading = true;
             bingServices = new BingServices();
             Images = new IncrementalCollection<ImageInfo>(LoadAsync);
         }
 
         readonly BingServices bingServices;
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public IncrementalCollection<ImageInfo> Images { get; }
+
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get => isLoading;
+            set
+            {
+                isLoading = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsLoading)));
+            }
+        }
+
 
         int loadCount = 0;
         private async Task<IEnumerable<ImageInfo>> LoadAsync()
@@ -26,7 +41,9 @@ namespace HappyDog.BingWallpaper.Views
             if (loadCount < 2)
             {
                 loadCount++;
-                return await bingServices.LoadAsync();
+                var data = await bingServices.LoadAsync();
+                IsLoading = false;
+                return data;
             }
             else
             {
